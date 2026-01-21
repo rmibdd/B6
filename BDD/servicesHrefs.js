89a775570b6f3bbe6976c32ef3df01aa0503f817
Charles M. Ramos <charles@example.com>
2026-01-21 16:06:59 +0800

    Commit updates

--- BDD/servicesHrefs.js
+++ BDD/servicesHrefs.js
@@ -3,7 +3,9 @@ const projectRequestUrl = "https://forms.office.com/r/AHPQAeL3Nj";
 const Policy1 = "https://rayomarmgt.sharepoint.com/:b:/s/WebsiteMaterials/IQD-8o-HExW-RY_LseAOO3U1AdlDaQmQq5k6rnME11n1Ggs?e=MtkstO";
 const Policy2 = "https://rayomarmgt.sharepoint.com/:b:/s/WebsiteMaterials/IQBf0abBP0QcS57GPAac86MLAR7dNGm0pXdnenzK2fyc3FQ?e=vcJdoJ";
 const Policy3 = "#"; // Placeholder link for Management Excellence Award
-const SPfile = "https://rayomarmgt-my.sharepoint.com/:f:/g/personal/charles_ramos_rayomar_com_ph/IgCw3AtMh_adQq5xbkroXJtoATqXCr6_mmMunyTAUQw4XAU?e=S2UwdZ"
+const SPfile = "https://rayomarmgt-my.sharepoint.com/:f:/g/personal/charles_ramos_rayomar_com_ph/IgAmsMoc2fdkQrBIdVaa7MosAW3Ib6JrRwgP93-8BoTPzXU?e=JsNvlw"
+const logosfile ="#"
+const templatefile ="#"
 //HREF navigation within the website
 const Homepage = "../Landingpage/LandingPage.html";
 // HREF navigation within the BDD section
