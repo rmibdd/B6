a29f0c863629e2c5c193bb8efdca61d8c8ecae8e
Charles M. Ramos <charles@example.com>
2026-01-28 15:45:26 +0800

    Updated

--- BDD/BDDLP.js
+++ BDD/BDDLP.js
@@ -1,13 +1,17 @@
 // Variable names for HREFs in ServicesLP.html
 const projectRequestUrl = "https://forms.office.com/r/AHPQAeL3Nj";
-const Policy1 = "https://rayomarmgt.sharepoint.com/:b:/s/WebsiteMaterials/IQD-8o-HExW-RY_LseAOO3U1AdlDaQmQq5k6rnME11n1Ggs?e=MtkstO";
-const Policy2 = "https://rayomarmgt.sharepoint.com/:b:/s/WebsiteMaterials/IQBf0abBP0QcS57GPAac86MLAR7dNGm0pXdnenzK2fyc3FQ?e=vcJdoJ";
+
+//Variable names for HREFs in PoliciesLP.html
+const Policy1 = "https://rayomarmgt-my.sharepoint.com/:b:/g/personal/charles_ramos_rayomar_com_ph/IQBCAkcFGc_FQZgBoYvehIt8Ac53KizNHP8vkbpfXrBUuDo?e=MWZuDb";
+const Policy2 = "https://rayomarmgt-my.sharepoint.com/:b:/g/personal/charles_ramos_rayomar_com_ph/IQADcXwwiJL_Q7jWbj29tHjNAVoriqJDebGBE0uufMkqBRg?e=VamsMx";
 const Policy3 = "#"; // Placeholder link for Management Excellence Award
+const PolicyPRF = "https://rayomarmgt-my.sharepoint.com/:b:/g/personal/charles_ramos_rayomar_com_ph/IQBxWfWmM4ykQ7iDSoATiInfARuqyYl-7rqYBkDf2maC99Y?e=UIP0FF"; // Placeholder link for Management Excellence Award
+const PolicyPTL = "https://rayomarmgt-my.sharepoint.com/:b:/g/personal/charles_ramos_rayomar_com_ph/IQDdZXGhg9xsQKD0FBkxOG_OAbWsagTUvbSmgidN_5AdCWs?e=7UJJNX"; // Placeholder link for Management Excellence Award
 
 
 //HREF navigation within the website
 const Homepage = "../Landingpage/LandingPage.html";
-const SPfile = "https://rayomarmgt-my.sharepoint.com/:f:/g/personal/charles_ramos_rayomar_com_ph/IgAmsMoc2fdkQrBIdVaa7MosAQY3GwxAOqqEBE-6ZGwTW9Q?e=sEUkoU"
+const SPfile = "https://rayomarmgt-my.sharepoint.com/:f:/g/personal/charles_ramos_rayomar_com_ph/IgAmsMoc2fdkQrBIdVaa7MosAbybUzj7-mWwnPsPhaDH18Y?e=9m4Lcq"
 const logosfile ="https://rayomarmgt-my.sharepoint.com/:f:/g/personal/charles_ramos_rayomar_com_ph/IgBonfPH8yR7RbFOnc-yFoVaASiqUzRn1mNpz19zBceTY28?e=fuWKoB"
 const templatefile ="#"
 const teamsfile ="MS Teams.html"
@@ -15,15 +19,15 @@ const chatgptfile ="ChatGPT.html"
 const fathomfile ="Fathom.html"
 //HREF navigation within MSTeams.html
 const teamsSignIn = "https://teams.microsoft.com";
-const teamsManual = "https://rayomarmgt-my.sharepoint.com/:b:/g/personal/charles_ramos_rayomar_com_ph/IQDaeTaMrGcmRZ3NwNB4ppZgAfe9Ph_WOcnmNhekcz7EQMQ?e=pjpyCP"
+const teamsManual = "https://rayomarmgt-my.sharepoint.com/:b:/g/personal/charles_ramos_rayomar_com_ph/IQD9_JEIYpU-T41haheBXaXAAeB0Wjs9sIYNJHxsWY-66EY?e=KebVbn"
 
 // HREF navigation within Fathom.html
-const fathomSignIn = "https://fathom.com"; // placeholder
-const fathomManual = "#"; // placeholder
+const fathomSignIn = "https://fathom.ai"; // placeholder
+const fathomManual = "https://rayomarmgt-my.sharepoint.com/:b:/g/personal/charles_ramos_rayomar_com_ph/IQAA9ET87Y3bSJSdguuelLHwARpxaTAGGkbAhk9OuiHtPYE?e=XJ2ZK1"; // placeholder
 
 // HREF navigation within ChatGPT.html
 const chatgptSignIn = "https://chat.openai.com";
-const chatgptManual = "#"; // placeholder
+const chatgptManual = "https://rayomarmgt-my.sharepoint.com/:b:/g/personal/charles_ramos_rayomar_com_ph/IQAc1xy0brKSRbiIZKnhZU6pAeba0OlUz9UOGUBbVnS6L80?e=gO5jnh"; // placeholder
 
 // HREF navigation within the BDD section
 const backToBDDMain = "BDDLP.html";
