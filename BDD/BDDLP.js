2913536fea5e8b1b768ecba09ae31e3d60202946
Charles M. Ramos <charles@example.com>
2026-01-29 10:30:47 +0800

    MigratetoNAS

--- BDD/BDDLP.js
+++ BDD/BDDLP.js
@@ -1,40 +1,45 @@
-// Variable names for HREFs in ServicesLP.html
-const projectRequestUrl = "https://forms.office.com/r/AHPQAeL3Nj";
-
-//Variable names for HREFs in PoliciesLP.html
-const Policy1 = "https://rayomarmgt-my.sharepoint.com/:b:/g/personal/charles_ramos_rayomar_com_ph/IQBCAkcFGc_FQZgBoYvehIt8Ac53KizNHP8vkbpfXrBUuDo?e=MWZuDb";
-const Policy2 = "https://rayomarmgt-my.sharepoint.com/:b:/g/personal/charles_ramos_rayomar_com_ph/IQADcXwwiJL_Q7jWbj29tHjNAVoriqJDebGBE0uufMkqBRg?e=VamsMx";
-const Policy3 = "#"; // Placeholder link for Management Excellence Award
-const PolicyPRF = "https://rayomarmgt-my.sharepoint.com/:b:/g/personal/charles_ramos_rayomar_com_ph/IQBxWfWmM4ykQ7iDSoATiInfARuqyYl-7rqYBkDf2maC99Y?e=UIP0FF"; // Placeholder link for Management Excellence Award
-const PolicyPTL = "https://rayomarmgt-my.sharepoint.com/:b:/g/personal/charles_ramos_rayomar_com_ph/IQDdZXGhg9xsQKD0FBkxOG_OAbWsagTUvbSmgidN_5AdCWs?e=7UJJNX"; // Placeholder link for Management Excellence Award
-
-
-//HREF navigation within the website
-const Homepage = "../Landingpage/LandingPage.html";
-const SPfile = "https://rayomarmgt-my.sharepoint.com/:f:/g/personal/charles_ramos_rayomar_com_ph/IgAmsMoc2fdkQrBIdVaa7MosAbybUzj7-mWwnPsPhaDH18Y?e=9m4Lcq"
-const logosfile ="https://rayomarmgt-my.sharepoint.com/:f:/g/personal/charles_ramos_rayomar_com_ph/IgBonfPH8yR7RbFOnc-yFoVaASiqUzRn1mNpz19zBceTY28?e=fuWKoB"
-const templatefile ="#"
-const teamsfile ="MS Teams.html"
-const chatgptfile ="ChatGPT.html"
-const fathomfile ="Fathom.html"
-//HREF navigation within MSTeams.html
-const teamsSignIn = "https://teams.microsoft.com";
-const teamsManual = "https://rayomarmgt-my.sharepoint.com/:b:/g/personal/charles_ramos_rayomar_com_ph/IQD9_JEIYpU-T41haheBXaXAAeB0Wjs9sIYNJHxsWY-66EY?e=KebVbn"
-
-// HREF navigation within Fathom.html
-const fathomSignIn = "https://fathom.ai"; // placeholder
-const fathomManual = "https://rayomarmgt-my.sharepoint.com/:b:/g/personal/charles_ramos_rayomar_com_ph/IQAA9ET87Y3bSJSdguuelLHwARpxaTAGGkbAhk9OuiHtPYE?e=XJ2ZK1"; // placeholder
-
-// HREF navigation within ChatGPT.html
-const chatgptSignIn = "https://chat.openai.com";
-const chatgptManual = "https://rayomarmgt-my.sharepoint.com/:b:/g/personal/charles_ramos_rayomar_com_ph/IQAc1xy0brKSRbiIZKnhZU6pAeba0OlUz9UOGUBbVnS6L80?e=gO5jnh"; // placeholder
-
 // HREF navigation within the BDD section
 const backToBDDMain = "BDDLP.html";
-const wixBookingUrl = "https://rmi-bdd.wixsite.com/rayomargroup-booking";
 const SMFiles = "FilesLP.html";
 const SMPolicies = "PoliciesLP.html";
 const SMServices = "ServicesLP.html";
+const teamsfile ="MS Teams.html"
+const chatgptfile ="ChatGPT.html"
+const fathomfile ="Fathom.html"
+
+// HREF navigation within WixBooking.html
+const wixBookingUrl = "https://rmi-bdd.wixsite.com/rayomargroup-booking";
+
+//HREF navigation within MSTeams.html
+const teamsSignIn = "https://teams.microsoft.com";
+const teamsManual = "https://gofile.me/70I49/zeGUqctZt"
+
+// HREF navigation within Fathom.html
+const fathomSignIn = "https://fathom.ai";
+const fathomManual = "https://gofile.me/70I49/0Fm0nOKYO"; 
+
+// HREF navigation within ChatGPT.html
+const chatgptSignIn = "https://chat.openai.com";
+const chatgptManual = "https://gofile.me/70I49/Mb5gaGMBg";
+
+
+//HREF navigation within the FilesLP.html
+const Homepage = "../Landingpage/LandingPage.html";
+const SPfile = "https://gofile.me/70I49/fjG0pC7ur";
+const logosfile = "https://gofile.me/70I49/x5WEWfOG2";
+const templatefile ="#"
+
+//Variable names for HREFs in PoliciesLP.html
+const PolicyLAD = "https://gofile.me/70I49/cljvKWYSl";
+const PolicySC = "https://gofile.me/70I49/64U1jyFlA";
+const PolicyMEA = "#"; // Placeholder link for Management Excellence Award
+const PolicyPRF = "https://gofile.me/70I49/tZrINxMQb";
+const PolicyPTL = "https://gofile.me/70I49/7pHEHiw6H"; 
+
+// Variable names for HREFs in ServicesLP.html
+const projectRequestUrl = "https://forms.office.com/r/AHPQAeL3Nj";
+
+
 
 // Search functionality
 const searchForm = document.querySelector('form.d-flex');
